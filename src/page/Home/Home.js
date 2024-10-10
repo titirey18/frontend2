568e3aac982da0b7713fc1a094a091f14e93815e
Thiago Soubielle <152506649+titirey18@users.noreply.github.com>
2024-10-10 11:38:31 +0200

    Update Home.js

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -5,53 +5,65 @@ import './Home.css'
 export const Home = async () => {
   const div = creatpage('Home')
 
-  const res = await back({
-    endpoint: '/juegos',
-    token: localStorage.getItem('token')
-  })
+  try {
 
-  const user = JSON.parse(localStorage.getItem('user')) || null
-
-  div.innerHTML = ''
-
-  for (const juego of res.juegos) {
-    const isFavorite = user && user.favoritos.includes(juego._id)
-    div.innerHTML += `
-      <div class="todos_los_juegos"> 
-        <h3>${juego.title}</h3>
-        <div>
-          <img src="${juego.img}" alt="${juego.title}"/>
-          ${
-            user
-              ? `<img 
-                  src="${
-                    isFavorite
-                      ? 'public/assets/corazon.png'
-                      : 'public/assets/corazon-vacio.png'
-                  }"
-                  class="favorite-icon"
-                  data-id="${juego._id}"
-                  alt="${
-                    isFavorite ? 'Quitar de favoritos' : 'Agregar a favoritos'
-                  }"
-                />`
-              : ''
-          }
-        </div>
-      </div>
-    `
-  }
-
-  if (user) {
-    document.querySelectorAll('.favorite-icon').forEach((icon) => {
-      icon.addEventListener('click', async (e) => {
-        const juegoId = e.target.getAttribute('data-id')
-        await toggleFavorite(juegoId)
-        Home()
-      })
+    
+    const res = await back({
+      endpoint: '/juegos',
+      token: localStorage.getItem('token')
     })
-  }
-}
+    
+    const user = JSON.parse(localStorage.getItem('user')) || null
+    
+    div.innerHTML = ''
+    
+    if(res && res.juegos && res.juegos.length > 0) {
+
+    for (const juego of res.juegos) {
+      const isFavorite = user && user.favoritos.includes(juego._id)
+      div.innerHTML += `
+      <div class="todos_los_juegos"> 
+      <h3>${juego.title}</h3>
+      <div>
+      <img src="${juego.img}" alt="${juego.title}"/>
+      ${
+            user
+            ? `<img 
+            src="${
+              isFavorite
+              ? 'public/assets/corazon.png'
+              : 'public/assets/corazon-vacio.png'
+              }"
+              class="favorite-icon"
+              data-id="${juego._id}"
+              alt="${
+                isFavorite ? 'Quitar de favoritos' : 'Agregar a favoritos'
+                }"
+                />`
+                : ''
+                }
+                </div>
+                </div>
+                `
+              }
+              
+              if (user) {
+                document.querySelectorAll('.favorite-icon').forEach((icon) => {
+                  icon.addEventListener('click', async (e) => {
+                    const juegoId = e.target.getAttribute('data-id')
+                    await toggleFavorite(juegoId)
+                    Home()
+                  })
+                })
+              }
+            } else {
+              div.innerHTML = "<p>No hay juegos disponibles en este momento..</p>"
+            }
+          } catch (error) {
+            div.innerHTML= "<p> Error al cargar los juegos.</p>"
+          }
+        }
+        
 
 const toggleFavorite = async (juegoId) => {
   const user = JSON.parse(localStorage.getItem('user')) || { favoritos: [] }
@@ -64,13 +76,17 @@ const toggleFavorite = async (juegoId) => {
   }
 
   const body = JSON.stringify({ favoritos: user.favoritos })
+  try {
 
-  await back({
-    endpoint: `/users/${user._id}`,
-    method: 'PUT',
-    body,
-    token: localStorage.getItem('token')
-  })
-
-  localStorage.setItem('user', JSON.stringify(user))
+    await back({
+      endpoint: `/users/${user._id}`,
+      method: 'PUT',
+      body,
+      token: localStorage.getItem('token')
+    })
+    
+    localStorage.setItem('user', JSON.stringify(user))
+  } catch (error) {
+    console.error('Error al actualizar favoritos:', error)
+  }
 }
